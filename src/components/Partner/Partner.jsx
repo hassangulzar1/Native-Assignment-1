3d4507360f87e6fc524c7eed771da03d01660198
hassangulzar1 <hassangulzar121@gmail.com>
2023-11-13 22:37:05 +0500

    partner page done

--- src/components/Partner/Partner.jsx
+++ src/components/Partner/Partner.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import {View, Text, Image, StyleSheet} from 'react-native';
 import BigButton from '../BigButton/BigButton';
-const Partner = () => {
+const Partner = props => {
   return (
     <>
       <View style={styles.mainContainer}>
         <View style={styles.leftArea}>
-          <Image
-            style={{width: 40, height: 40}}
-            source={require('../../assets/alan.png')}
-          />
+          <Image style={{width: 40, height: 40}} source={props.image} />
 
           <View style={{marginStart: 10}}>
-            <Text style={styles.titleText}>Alanlove</Text>
-            <Text style={styles.descText}>Travel Blogger</Text>
+            <Text style={styles.titleText}>{props.title}</Text>
+            <Text style={styles.descText}>{props.desc}</Text>
           </View>
         </View>
-        <BigButton color="rgba(3, 115, 243, 1)" text="Edit profile" />
+        {!props.forwardArrow && (
+          <BigButton color="rgba(3, 115, 243, 1)" text="Edit profile" />
+        )}
+        {props.forwardArrow && (
+          <Image source={require('../../assets/forwardArrow.png')} />
+        )}
       </View>
-      <Image source={require('../../assets/underline.png')} />
-      {/* <View style={styles.horizontalLine} /> */}
+      <Image
+        style={styles.horizontalLine}
+        source={require('../../assets/underline.png')}
+      />
     </>
   );
 };
@@ -28,6 +32,7 @@ export default Partner;
 
 const styles = StyleSheet.create({
   mainContainer: {
+    marginVertical: 10,
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -36,12 +41,11 @@ const styles = StyleSheet.create({
   leftArea: {
     display: 'flex',
     flexDirection: 'row',
+    alignItems: 'center',
   },
   horizontalLine: {
-    width: '100%',
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-    marginVertical: 10,
+    width: 330,
+    marginTop: 10,
   },
   titleText: {
     fontFamily: 'Poppins-Bold',
--- src/screens/7- Chart/Chart.jsx
+++ src/screens/7- Chart/Chart.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, FlatList, Image, Text, StyleSheet} from 'react-native';
 import AuthHeader from '../../components/AuthHeader/AuthHeader';
 import Partner from '../../components/Partner/Partner';
+
+const PartnerData = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-6766',
+    image: require('../../assets/alan.png'),
+    title: 'Alanlove',
+    desc: 'Travel Blogger',
+  },
+  {
+    id: 'bd7acbea-4544-46c2-aed5-6766',
+    image: require('../../assets/charlotte.png'),
+    title: 'Charlotte',
+    desc: 'Chief Travel Experiencer',
+    forwardArrow: true,
+  },
+  {
+    id: 'bd7acbea-4544-46c2-ae234d5-6766',
+    image: require('../../assets/evangiline.png'),
+    title: 'Evangeline',
+    desc: 'Chief Travel Experiencer',
+    forwardArrow: true,
+  },
+  {
+    id: 'bd7acbea-4544-46c2-aed5-345',
+    image: require('../../assets/geraldine.png'),
+    title: 'Geraldine',
+    desc: 'Private tour guide',
+    forwardArrow: true,
+  },
+  {
+    id: 'bd7acbea-4544-46c2-aed5-34543',
+    image: require('../../assets/prudence.png'),
+    title: 'Prudence',
+    desc: 'Travel youtube master',
+    forwardArrow: true,
+  },
+];
+
 const Chart = () => {
   return (
     <View style={{flex: 1, backgroundColor: '#fafafa'}}>
@@ -9,10 +47,46 @@ const Chart = () => {
         <AuthHeader title="Your Partner" />
       </View>
       <View style={{flex: 3, marginHorizontal: 15}}>
-        <Partner />
+        <FlatList
+          data={PartnerData}
+          renderItem={({item}) => (
+            <Partner
+              desc={item.desc}
+              title={item.title}
+              image={item.image}
+              forwardArrow={item.forwardArrow}
+              keyExtractor={item => item.id}
+            />
+          )}
+        />
+      </View>
+
+      <View style={styles.logoutBtn}>
+        <Image
+          style={{width: 20, height: 20}}
+          source={require('../../assets/logout.png')}
+        />
+        <Text style={styles.logoutBtnText}>Log out</Text>
       </View>
     </View>
   );
 };
 
 export default Chart;
+
+const styles = StyleSheet.create({
+  logoutBtn: {
+    flex: 0.5,
+    marginHorizontal: 15,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  logoutBtnText: {
+    fontFamily: 'Poppins-Bold',
+    color: 'rgba(3, 115, 243, 1)',
+    lineHeight: 26.32,
+    marginHorizontal: 8,
+    marginTop: 2.5,
+  },
+});
